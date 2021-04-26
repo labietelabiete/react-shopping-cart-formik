57b410d302e003e06d9978f193897c4c251fa03a
Dani Lucaci <danilucaci@gmail.com>
2021-04-26 10:33:27 +0200

    feat: add formik form solution

--- src/components/NewProductForm/NewProductForm.js
+++ src/components/NewProductForm/NewProductForm.js
@@ -87,7 +87,94 @@ function NewProductForm({ toggleNewProductForm, saveNewProduct }) {
                 hasErrorMessage={touched.title}
                 errorMessage={errors.title}
               />
-              {/* the other fields... */}
+              <Input
+                type="number"
+                label="Product price"
+                id="price"
+                value={values.price}
+                placeholder="Product price"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.price}
+                errorMessage={errors.price}
+              />
+              <Input
+                type="text"
+                label="Product image url"
+                id="img"
+                value={values.img}
+                placeholder="Product image url"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.img}
+                errorMessage={errors.img}
+              />
+              <Input
+                type="text"
+                label="Short description"
+                id="shortDescription"
+                value={values.shortDescription}
+                placeholder="Short description"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.shortDescription}
+                errorMessage={errors.shortDescription}
+              />
+              <Input
+                type="text"
+                label="Long description"
+                id="longDescription"
+                value={values.longDescription}
+                placeholder="Long description"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.longDescription}
+                errorMessage={errors.longDescription}
+              />
+              <Input
+                type="number"
+                label="Units in stock"
+                id="unitsInStock"
+                value={values.unitsInStock}
+                placeholder="Units in stock"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.unitsInStock}
+                errorMessage={errors.unitsInStock}
+              />
+              <Input
+                type="text"
+                label="Author first name"
+                id="authorFirstName"
+                value={values.authorFirstName}
+                placeholder="Author first name"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.authorFirstName}
+                errorMessage={errors.authorFirstName}
+              />
+              <Input
+                type="text"
+                label="Author last name"
+                id="authorLastName"
+                value={values.authorLastName}
+                placeholder="Author last name"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.authorLastName}
+                errorMessage={errors.authorLastName}
+              />
+              <Input
+                type="email"
+                label="Author email"
+                id="authorEmail"
+                value={values.authorEmail}
+                placeholder="Author email"
+                handleChange={handleChange}
+                handleBlur={handleBlur}
+                hasErrorMessage={touched.authorEmail}
+                errorMessage={errors.authorEmail}
+              />
               <Button submitButton block disabled={isValidating || !isValid}>
                 Submit
               </Button>
